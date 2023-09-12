9c4330aa65cb4e6ef28b8a1ce1dcf7f31c7c6249
taojinsheng123 <286465927@qq.com>
2023-09-12 23:13:17 +0800

    11

---
+++ src/main/java/com/tao/util/DeskTopUtil.java
@@ -0,0 +1,26 @@
+package com.tao.util;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class DeskTopUtil {
+
+    public static void main(String[] args) {
+        backtoDeskTop();
+    }
+    public static void  backtoDeskTop(){
+        try {
+            Robot robot = new Robot();
+
+            // 模拟按下并释放'Windows'键
+            robot.keyPress(KeyEvent.VK_WINDOWS);
+            robot.keyPress(KeyEvent.VK_D);
+            robot.keyRelease(KeyEvent.VK_WINDOWS);
+            // 模拟按下并释放'D'键
+            robot.keyRelease(KeyEvent.VK_D);
+
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+    }
+}
