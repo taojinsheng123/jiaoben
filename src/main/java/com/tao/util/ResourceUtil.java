df940c4d373b7361d6a5c8c2a62ea7774dfa5b5b
taojinsheng <17701048244@189.cn>
2023-09-13 00:24:24 +0800

    dddddd

--- src/main/java/com/tao/util/ResourceUtil.java
+++ src/main/java/com/tao/util/ResourceUtil.java
@@ -5,7 +5,7 @@ public class ResourceUtil {
     private static String path=ResourceUtil.class.getClassLoader().getResource("").getPath();
 
     public static String getResourcesPath(){
-
+        //
         return path;
     }
 }
